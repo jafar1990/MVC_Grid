3d8054b9f763dc8c8a3c269a244108de74278d03
jafar k <jafarkarakkuth@gmail.com>
2020-07-29 20:54:42 +0530

    Commit Test2

--- MVC_Grid/Controllers/HomeController.cs
+++ MVC_Grid/Controllers/HomeController.cs
@@ -12,20 +12,13 @@ namespace MVC_Grid.Controllers
         tbl_studentModel db = new tbl_studentModel();
         public ActionResult Create()
         {
-            ////var values = db.tbl_student.ToList();
 
-            //ViewModel vm = new ViewModel();
-
-            //vm.tbl_students = null;
-
-            //vm.GridModels = db.tbl_student.ToList();
 
             return View();
         }
 
         public ActionResult Grid()
         {
-            //var values = db.GridModels.ToList();
 
             return View();
         }
